74f7286a46789b1befee325891a3a1855c8ed8f1
polifr <polifr@gmail.com>
2026-04-10 14:45:18 +0200

    Creazione test elementare per verifica framework
    Riferimento:
    https://github.com/keycloak/keycloak/blob/main/test-framework/docs/GETTING_STARTED.md

---
+++ src/test/java/org/keycloak/BasicKeycloakRealmTest.java
@@ -0,0 +1,20 @@
+package org.keycloak;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.keycloak.testframework.annotations.InjectRealm;
+import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
+import org.keycloak.testframework.realm.ManagedRealm;
+
+@KeycloakIntegrationTest
+class BasicKeycloakRealmTest {
+
+	@InjectRealm
+	ManagedRealm managedRealm;
+
+	@Test
+	void managedRealmTest() {
+		assertNotNull(managedRealm.admin().toRepresentation());
+	}
+}
