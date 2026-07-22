c57a7b659de61d51c2f2dc027c1eaf2dca436ee0
polifr <polifr@gmail.com>
2026-07-22 23:00:59 +0200

    Fix import per assertNotNull

--- src/test/java/org/keycloak/BasicKeycloakRealmTest.java
+++ src/test/java/org/keycloak/BasicKeycloakRealmTest.java
@@ -1,6 +1,7 @@
 package org.keycloak;
 
-import static org.junit.Assert.assertNotNull;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.keycloak.testframework.annotations.InjectRealm;
--- src/test/java/org/keycloak/broker/spid/realm/SpidKeycloakRealmTest.java
+++ src/test/java/org/keycloak/broker/spid/realm/SpidKeycloakRealmTest.java
@@ -1,7 +1,7 @@
 package org.keycloak.broker.spid.realm;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.keycloak.testframework.annotations.InjectRealm;
