0c9cb91e77f625a8cbb35245c51a1909fc999750
polifr <polifr@gmail.com>
2026-08-02 23:42:45 +0200

    Avanzamento test junit su configurazione realm

--- src/test/java/org/keycloak/broker/spid/realm/SpidKeycloakRealmTest.java
+++ src/test/java/org/keycloak/broker/spid/realm/SpidKeycloakRealmTest.java
@@ -1,11 +1,18 @@
 package org.keycloak.broker.spid.realm;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
+import org.jboss.logging.Logger;
 import org.junit.jupiter.api.Test;
 import org.keycloak.broker.spid.configuration.SpidRealmConfig;
 import org.keycloak.broker.spid.configuration.SpidSamlKeycloakServerConfig;
+import org.keycloak.representations.idm.IdentityProviderMapperRepresentation;
+import org.keycloak.representations.idm.IdentityProviderRepresentation;
 import org.keycloak.testframework.annotations.InjectRealm;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
 import org.keycloak.testframework.injection.LifeCycle;
@@ -24,6 +31,8 @@ import org.keycloak.testframework.realm.ManagedRealm;
 @KeycloakIntegrationTest(config = SpidSamlKeycloakServerConfig.class)
 public class SpidKeycloakRealmTest {
 
+    private static final Logger logger = Logger.getLogger(SpidKeycloakRealmTest.class);
+
     /**
      * Riferimento al realm configurato tramite {@link SpidRealmConfig}
      */
@@ -34,7 +43,7 @@ public class SpidKeycloakRealmTest {
      * Verifica che il realm e' stato iniettato
      */
     @Test
-    final void spidRealShouldBeInjected() {
+    final void spidRealmShouldBeInjected() {
         assertNotNull(spidRealm, "spidRealm not injected");
     }
 
@@ -44,5 +53,27 @@ public class SpidKeycloakRealmTest {
     @Test
     final void spidRealmNameIsSpid() {
         assertEquals("spid", spidRealm.getName());
+    }
+
+    @Test
+    final void spidIdentityProviderIsConfigured() {
+        List<IdentityProviderRepresentation> identityProviders = spidRealm.getCreatedRepresentation().getIdentityProviders();
+        assertNotNull(identityProviders, "SPID realm identityProviders is null.");
+        assertFalse(identityProviders.isEmpty(), "SPID realm identityProviders is empty.");
+        List<String> aliases = identityProviders.stream().map(IdentityProviderRepresentation::getAlias).toList();
+        logger.info("SPID realm Identity Providers: " + aliases.toString());
+        assertTrue(
+                aliases.contains("spid-spid-sp-test"),
+                () -> "SPID realm Identity Providers: " + aliases + "; spid-spid-sp-test not found."
+        );
+        IdentityProviderRepresentation ipr = identityProviders.stream().filter(ip -> ip.getAlias().equals("spid-spid-sp-test")).findFirst().get();
+        assertTrue(ipr.isTrustEmail(), "Attribute Trust Email has to be true.");
+    }
+
+    @Test
+    final void spidIdentityProviderMappersAreConfigured() {
+        List<IdentityProviderMapperRepresentation> identityProviderMappers = spidRealm.getCreatedRepresentation().getIdentityProviderMappers();
+        assertNotNull(identityProviderMappers, "SPID realm identityProviderMappers is null.");
+        assertFalse(identityProviderMappers.isEmpty(), "SPID realm identityProviderMappers is empty.");
     }
 }
