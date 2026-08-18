5c6facd638bd7cbb224bd581aa3326c5fd7f6d7c
polifr <polifr@gmail.com>
2026-08-19 00:16:20 +0200

    Verifiche su contenuto url estratti dal client (xml e json)
    Signed-off-by: polifr <polifr@gmail.com>

--- src/test/java/org/keycloak/broker/spid/realm/SpidKeycloakRealmTest.java
+++ src/test/java/org/keycloak/broker/spid/realm/SpidKeycloakRealmTest.java
@@ -4,14 +4,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.junit.jupiter.api.Assertions.fail;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jboss.logging.Logger;
@@ -27,7 +32,14 @@ import org.keycloak.testframework.annotations.InjectRealm;
 import org.keycloak.testframework.annotations.KeycloakIntegrationTest;
 import org.keycloak.testframework.injection.LifeCycle;
 import org.keycloak.testframework.realm.ManagedRealm;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 
@@ -45,8 +57,11 @@ import com.github.tomakehurst.wiremock.client.WireMock;
 public class SpidKeycloakRealmTest {
 
     private static final List<String> IDENTITY_PROVIDER_ALIASES = List.of("spid-spid-sp-test", "spid-demo");
+    private static final String SPID_REALM_BASE_URL = "http://localhost:8080/realms/spid";
 
     private static final Logger logger = Logger.getLogger(SpidKeycloakRealmTest.class);
+    private static final ObjectMapper MAPPER = JsonMapper.builder().findAndAddModules().build();
+    private static 
 
     /**
      * Riferimento al realm configurato tramite {@link SpidRealmConfig}
@@ -96,17 +111,26 @@ public class SpidKeycloakRealmTest {
 
     @Test
     final void spidRealmServiceProviderEntityIdIsReadable() throws Exception {
-        this.checkIfUrlIsReadable("http://localhost:8080/realms/spid");
+        String response = this.checkIfUrlIsReadable(SPID_REALM_BASE_URL);
+        assertTrue(StringUtils.isNotEmpty(response), "Response body is null or empty");
+        this.checkValidJson(response);
+        // TODO Ulteriori verifiche su contenuto json
     }
 
     @Test
     final void spidRealmSamlDescriptorIsReadable() throws Exception {
-        this.checkIfUrlIsReadable("http://localhost:8080/realms/spid/protocol/saml/descriptor");
+        String response = this.checkIfUrlIsReadable(SPID_REALM_BASE_URL + "/protocol/saml/descriptor");
+        assertTrue(StringUtils.isNotEmpty(response), "Response body is null or empty");
+        this.checkValidXml(response);
+        // TODO Ulteriori verifiche su contenuto xml
     }
 
     @Test
     final void spidRealmOpenIdConfigurationIsReadable() throws Exception {
-        this.checkIfUrlIsReadable("http://localhost:8080/realms/spid/.well-known/openid-configuration");
+        String response = this.checkIfUrlIsReadable(SPID_REALM_BASE_URL + "/.well-known/openid-configuration");
+        assertTrue(StringUtils.isNotEmpty(response), "Response body is null or empty");
+        this.checkValidJson(response);
+        // TODO Ulteriori verifiche su contenuto json
     }
 
     @Test
@@ -122,13 +146,31 @@ public class SpidKeycloakRealmTest {
         }
     }
 
-    private void checkIdentityProviders(List<IdentityProviderRepresentation> identityProvider, String identityProviderAlias) throws Exception {
-        IdentityProviderRepresentation ip = identityProvider.stream()
+    /**
+     * Verifica che per l'alias fornito sia presente un identity provider e che l'url a cui
+     * e' esposto il metadata descriptor sia raggiungibile.
+     *
+     * @param identityProviders Elenco degli identity providers del realm
+     * @param identityProviderAlias Alias dell'identity provider
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    private void checkIdentityProviders(List<IdentityProviderRepresentation> identityProviders, String identityProviderAlias) throws InterruptedException, IOException {
+        IdentityProviderRepresentation ip = identityProviders.stream()
                 .filter(p -> p.getAlias().equals(identityProviderAlias))
                 .findFirst().orElseGet(() -> Assertions.fail("No identity provider configured in SPID realm for alias " + identityProviderAlias));
         assertTrue(ip.isTrustEmail(), "Attribute Trust Email has to be true.");
-        
-        this.checkIfUrlIsReadable(ip.getConfig().get("metadataDescriptorUrl"));
+
+        String response = this.checkIfUrlIsReadable(ip.getConfig().get("metadataDescriptorUrl"));
+        assertTrue(StringUtils.isNotEmpty(response), "Response body is null or empty");
+        this.checkValidXml(response);
+        // TODO Ulteriori verifiche su contenuto xml
+
+        String endpointDescriptorUrl = SPID_REALM_BASE_URL + "/broker/" + identityProviderAlias + "/endpoint/descriptor";
+        response = this.checkIfUrlIsReadable(endpointDescriptorUrl);
+        assertTrue(StringUtils.isNotEmpty(response), "Response body is null or empty");
+        this.checkValidXml(response);
+        // TODO Ulteriori verifiche su contenuto xml
     }
 
     @Test
@@ -141,14 +183,30 @@ public class SpidKeycloakRealmTest {
             this.checkIdentityProviderMappers(identityProviderMappers, i);
         }
     }
-    
+
+    /**
+     * Verifica che per l'alias fornito sia presente almeno un mapper 
+     * 
+     * @param identityProviderMappers Elenco dei mappers del realm
+     * @param identityProviderAlias Alias dell'identity provider
+     */
     private void checkIdentityProviderMappers(List<IdentityProviderMapperRepresentation> identityProviderMappers, String identityProviderAlias) {
         List<IdentityProviderMapperRepresentation> ipm = identityProviderMappers.stream()
                 .filter(p -> p.getIdentityProviderAlias().equals(identityProviderAlias)).toList();
         assertFalse(ipm.isEmpty(), "No identity provider mappers configured in SPID realm for alias " + identityProviderAlias);
     }
 
-    private String checkIfUrlIsReadable(String requestUrl) throws Exception {
+    /**
+     * Verifica se l'url fornito come parametro e' leggibile in get, ossia se la chiamata
+     * riceve uno status con codice strettamente minore di 400. Il metodo restituisce il
+     * body della risposta, sotto forma di stringa.
+     *
+     * @param requestUrl Url da interrogare
+     * @return Body della risposta, se ricevuta con status < 400
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    private String checkIfUrlIsReadable(String requestUrl) throws InterruptedException, IOException {
         HttpResponse<String> response = httpClient.send(
                 HttpRequest.newBuilder()
                         .uri(URI.create(requestUrl))
@@ -157,7 +215,31 @@ public class SpidKeycloakRealmTest {
                 HttpResponse.BodyHandlers.ofString()
                 );
         assertTrue(response.statusCode() < 400, "Response status calling " + requestUrl + " is not ok: " + response.statusCode());
-        assertTrue(StringUtils.isNotEmpty(response.body()), "Response body is null or empty");
         return response.body();
+    }
+
+    private JsonNode checkValidJson(String json) {
+        try {
+            JsonNode node = MAPPER.readTree(json);
+            assertNotNull(node, "Json parsing produced null node.");
+            assertTrue(node.isObject(), "Json parsing produced a not object node.");
+            return node;
+        } catch (JsonProcessingException ex) {
+            return fail("Exception parsing json string: " + ex.getMessage());
+        }
+    }
+
+    private boolean checkValidXml(String xml) {
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+
+            factory.newSAXParser().parse(new InputSource(new StringReader(xml)), new DefaultHandler());
+            return true;
+        } catch (SAXException | IOException | ParserConfigurationException ex) {
+            return fail("Exception parsing xml string: " + ex.getMessage());
+        }
     }
 }
